2451eba1b8f2f348c736d7117861c87177dcbdd5
Andrey <unf.funeral@gmail.com>
2014-05-07 00:23:11 +0400

    fix for case when simple scroll transforming to overscroll (when touching down from middle/top) + added License (:

--- OverScrollListView/OverScrollListView/src/ru/suvitruf/overscrolllistview/OverscrollListView.java
+++ OverScrollListView/OverScrollListView/src/ru/suvitruf/overscrolllistview/OverscrollListView.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2014 Andrey Apanasik (aka Suvitruf)
+* Github https://github.com/Suvitruf/Android-sdk-examples/tree/master/OverScrollListView
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package ru.suvitruf.overscrolllistview;
 
 
@@ -6,6 +22,7 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.ListView;
@@ -119,21 +136,49 @@ public class OverscrollListView extends ListView {
 			offset = 0;
 		} else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
 
+			int oldLastScroll = lastScroll;
 			lastScroll = (int) (firstY - newY);
 
 
-			// fix for case when simple scroll transform to overscroll
-			if (lastScroll < 0) {
+			// fix for case when simple scroll transforming to overscroll (when touching down from middle/bottom)
+			if (lastScroll < 0) 
 				if(getScrollY() == 0){
+					// we should get offset for future, if we started scrolling from middle/bottom
 					if (getFirstVisiblePosition() == 0 && getChildAt(0) != null && getChildAt(0).getTop() == 0) {
-						offset = lastScroll;
+						offset = oldLastScroll;
 						lastScroll=-1;
 					}
 				}
 				else
 					lastScroll-=offset;
-					
+			
+			// fix for case when simple scroll transforming to overscroll (when touching down from middle/top)
+			if (lastScroll >= 0) {
+				View v = getChildAt(getChildCount() - 1);
+				if(getScrollY() == 0){
+					// we should get offset for future, if we started scrolling from middle/top
+					// here we have 2 cases:
+					// 1: when we started scrolling from bottom position when LastVisibleItem is full visible
+					// 2: when we started scrolling from middle/top position when LastVisibleItem is not full visible
+					if (getLastVisiblePosition() == getCount() - 1) {
+						if (offset == 0)
+							if(v.getTop() + v.getHeight() == getHeight())
+								offset = 0;
+							else
+								offset = oldLastScroll + v.getHeight();
+						
+						if(lastScroll - offset < 0)
+							lastScroll = 0;
+						else
+							lastScroll = lastScroll - offset;
+					}		
+				}
+				else{	
+					lastScroll = lastScroll - offset;
+				}
 			}
+					
+			
 			
 			if(mSlowEffect){
 				if (getOverScrollYWithSlow(lastScroll) < -mMaxYOverscrollDistance)
@@ -156,7 +201,7 @@ public class OverscrollListView extends ListView {
 
 				}
 			} else {
-				if (getLastVisiblePosition() == getCount() - 1) {
+				if (getLastVisiblePosition() == getCount() - 1 && lastScroll != 0) {
 					pullDown(lastScroll, 0);
 				
 					return true;
